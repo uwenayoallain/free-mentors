be808642bb492d3dc602ebbc0949e5c6efa48e6d
Alain Pacifique UWENAYO <uwenayoallain@gmail.com>
2025-02-27 11:54:55 +0200

    Added protected route helper method

---
+++ frontend/src/utils/ProtectedRoute.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+
+interface ProtectedRouteProps {
+    children: React.ReactNode;
+    requiredRole?: string;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
+    children,
+    requiredRole
+}) => {
+    const { isAuthenticated, user } = useSelector((state: RootState) => state.auth);
+    const location = useLocation();
+
+    if (!isAuthenticated) {
+        return <Navigate to="/auth/login" state={ { from: location } } replace />;
+    }
+
+    if (requiredRole && user?.role !== requiredRole) {
+        return <Navigate to="/" replace />;
+    }
+
+    return <>{ children }</>;
+};
+
+export default ProtectedRoute;
