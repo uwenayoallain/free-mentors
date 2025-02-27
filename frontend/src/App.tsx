94ac065baadd4497d097f3ca7d132d1635b8445e
Alain Pacifique UWENAYO <uwenayoallain@gmail.com>
2025-02-27 11:56:20 +0200

    Updated App.tsx, index.css and main.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,65 @@
-import React from 'react'
-import { Container, Typography, Button } from "@mui/material"
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { Provider } from 'react-redux';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { store } from './store';
+import theme from './theme/theme';
 
+// Pages
+import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
+import MentorsPage from './pages/MentorsPage';
+import MentorDetailPage from './pages/MentorDetailPage';
+import SessionsPage from './pages/SessionsPage';
+import ProfilePage from './pages/ProfilePage';
+import NotFoundPage from './pages/NotFoundPage';
+
+// Auth protection
+import ProtectedRoute from './utils/ProtectedRoute';
+
+const App: React.FC = () => {
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h4" gutterBottom>Hello, Free mentors</Typography>
-      <Button variant="outlined" centerRipple color="primary">Click Me</Button>
-    </Container>
-  )
-}
+    <Provider store={ store }>
+      <ThemeProvider theme={ theme }>
+        <LocalizationProvider dateAdapter={ AdapterDateFns }>
+          <CssBaseline />
+          <Router>
+            <Routes>
+              {/* Public Routes */ }
+              <Route path="/" element={ <HomePage /> } />
+              <Route path="/auth/:authType" element={ <AuthPage /> } />
+              <Route path="/mentors" element={ <MentorsPage /> } />
+              <Route path="/mentors/:mentorId" element={ <MentorDetailPage /> } />
 
-export default App
+              {/* Protected Routes */ }
+              <Route
+                path="/sessions"
+                element={
+                  <ProtectedRoute>
+                    <SessionsPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <ProfilePage />
+                  </ProtectedRoute>
+                }
+              />
+
+              {/* Not Found */ }
+              <Route path="/404" element={ <NotFoundPage /> } />
+              <Route path="*" element={ <Navigate to="/404" replace /> } />
+            </Routes>
+          </Router>
+        </LocalizationProvider>
+      </ThemeProvider>
+    </Provider>
+  );
+};
+
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ThemeProvider } from '@emotion/react'
-import theme from './theme.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={ theme }>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 )
